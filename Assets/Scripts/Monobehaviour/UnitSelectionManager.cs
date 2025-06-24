f10c527577bb97e7fae5f1e505f0680f35ca5fa4
Neur0mancer <plsportz@gmail.com>
2025-06-24 17:32:55 +0200

    Backup
    Mouse selection added
    Unit movement changed

--- Assets/Scripts/Monobehaviour/UnitSelectionManager.cs
+++ Assets/Scripts/Monobehaviour/UnitSelectionManager.cs
@@ -1,17 +1,67 @@
+using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 public class UnitSelectionManager : MonoBehaviour
 {
+    public static UnitSelectionManager Instance { get; private set; }
+
+    public event EventHandler OnSelectionStart;
+    public event EventHandler OnSelectionEnd;
+
+    private Vector2 selectionStartMousePosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionStartMousePosition = Input.mousePosition;
+            OnSelectionStart?.Invoke(this, EventArgs.Empty);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 selectionEndMousePosition = Input.mousePosition;
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager); 
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp); // All selected units to array
+            for(int i =0; i < entityArray.Length; i++)
+            {
+                entityManager.SetComponentEnabled<Selected>(entityArray[i], false); //Disabling all selected units
+            }
+
+            entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
+
+            Rect selectionAreaRect = GetSelectionAreaRect();
+            entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<LocalTransform> localTransformArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+
+            for (int i = 0; i < localTransformArray.Length; i++)
+            {
+                LocalTransform unitLocalTransform = localTransformArray[i];
+                Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+                if (selectionAreaRect.Contains(unitScreenPosition))
+                {
+                    entityManager.SetComponentEnabled<Selected>(entityArray[i], true); //Selecting all units in area
+                }
+            }
+
+            OnSelectionEnd?.Invoke(this, EventArgs.Empty);
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
             Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
 
             EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<UnitMover>().Build(entityManager);
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<UnitMover, Selected>().Build(entityManager);
 
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
             NativeArray<UnitMover> unitMoverArray = entityQuery.ToComponentDataArray<UnitMover>(Allocator.Temp);
@@ -25,4 +75,22 @@ public class UnitSelectionManager : MonoBehaviour
             entityQuery.CopyFromComponentDataArray(unitMoverArray);
         }
     }
+
+    public Rect GetSelectionAreaRect()
+    {
+        Vector2 selectionEndMousePosition = Input.mousePosition;
+
+        Vector2 lowerLeftCorner = new Vector2(
+            Mathf.Min(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Min(selectionStartMousePosition.y, selectionEndMousePosition.y));
+        Vector2 uppreRightCorner = new Vector2(
+            Mathf.Max(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y));
+        return new Rect(
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            uppreRightCorner.x - lowerLeftCorner.x,
+            uppreRightCorner.y - lowerLeftCorner.y);
+    }
+
 }
--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -9,24 +9,57 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        UnitMoverJob unitMoverJob = new UnitMoverJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime,
+        };
+        unitMoverJob.ScheduleParallel();
+        /*
         foreach ((
-            RefRW<LocalTransform>  localTransforn,
+            RefRW<LocalTransform>  localTransform,
             RefRO<UnitMover> unitMover,
             RefRW < PhysicsVelocity> physicsVelocity)
             in SystemAPI.Query<
-            RefRW<LocalTransform>, 
-            RefRO<UnitMover>,
-            RefRW<PhysicsVelocity>>())
+                RefRW<LocalTransform>, 
+                RefRO<UnitMover>,
+                RefRW<PhysicsVelocity>>())
         {
-            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransforn.ValueRO.Position;
+            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
             moveDirection = math.normalize(moveDirection);
 
-            localTransforn.ValueRW.Rotation = math.slerp(localTransforn.ValueRO.Rotation, 
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, 
                 quaternion.LookRotation(moveDirection, math.up()), 
                 SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
 
             physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.moveSpeed;
             //localTransforn.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+        
         }
+        */
+    }
+}
+
+[BurstCompile]
+public partial struct UnitMoverJob : IJobEntity
+{
+    public float deltaTime;
+    public void Execute(ref LocalTransform localTransform, in UnitMover unitMover, ref PhysicsVelocity physicsVelocity)
+    {
+        float3 moveDirection = unitMover.targetPosition - localTransform.Position;
+        float reachedTargetDistances = 2f;
+        if(math.lengthsq(moveDirection) < reachedTargetDistances)
+        {
+            physicsVelocity.Linear = float3.zero;
+            physicsVelocity.Angular = float3.zero;
+            return;
+        }
+        moveDirection = math.normalize(moveDirection);
+
+        localTransform.Rotation = math.slerp(localTransform.Rotation,
+            quaternion.LookRotation(moveDirection, math.up()),
+            deltaTime  * unitMover.rotationSpeed);
+
+        physicsVelocity.Linear = moveDirection * unitMover.moveSpeed;
+        physicsVelocity.Angular = float3.zero;
     }
 }
